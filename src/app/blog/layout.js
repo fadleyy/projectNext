17fd1ecca9ef1c177699aaac99971c28fd89cde5
Muhamad Fadli <mf085891982215@gmail.com>
2024-01-16 23:08:03 +0700

    create blog layout :fist:

---
+++ src/app/blog/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Bloglayouts = ({ children }) => {
+  return (
+    <div>
+      <h1>Blog layouts</h1>
+      {children}
+    </div>
+  );
+};
+
+export default Bloglayouts;
