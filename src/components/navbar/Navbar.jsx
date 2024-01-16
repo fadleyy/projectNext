d1680550a1cc80744c8bded8b8eb443c991b9ec3
Muhamad Fadli <mf085891982215@gmail.com>
2024-01-16 23:53:20 +0700

    create components Links :sleepy:

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 import React from "react";
+import Links from "./link/Links";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>Navbar</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
