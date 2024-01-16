2aec0539a891eaf8d0fba17f6db8c41f30aa755e
Muhamad Fadli <mf085891982215@gmail.com>
2024-01-16 23:26:49 +0700

    create container import in components>(index.js) :+1:

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Footer, Navbar } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <h1>This is the main layout</h1>
         {children}
+        <Footer />
       </body>
     </html>
   );
