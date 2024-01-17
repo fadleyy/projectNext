e31ea0da6e516ab9b89f436d54fa1b92e6673f6f
Muhamad Fadli <mf085891982215@gmail.com>
2024-01-17 17:39:38 +0700

    create navbar responsive and global responsive :smiley:

--- src/components/navbar/link/Links.jsx
+++ src/components/navbar/link/Links.jsx
@@ -1,43 +1,58 @@
-import Link from "next/link";
-import React from "react";
-import style from "./links.module.css";
+"use client";
+import React, { useState } from "react";
+import styles from "./links.module.css";
 import Navlink from "./navLink/Navlink";
-
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
 const Links = () => {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState(false);
 
   // Temporary
   const session = true;
-  const isAdmin = true;
+  const isAdmin = false;
   return (
-    <div className={style.links}>
-      {links.map((link) => (
-        <Navlink item={link} key={link.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && <Navlink item={{ title: "Admin", path: "/admin" }} />}
-          <button>Logout</button>
-        </>
-      ) : (
-        <Navlink item={{ title: "Login", path: "/login" }} />
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Navlink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <Navlink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <Navlink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button
+        className={styles.menuBtn}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <Navlink item={link} key={link.title} />
+          ))}
+        </div>
       )}
     </div>
   );
