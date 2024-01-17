7a0a5b86418dbeaf814337154e1a570ea8d89b3c
Muhamad Fadli <mf085891982215@gmail.com>
2024-01-17 17:58:41 +0700

    import image external and internal :+1:

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,19 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
-  return <div>About Page</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/14801450/pexels-photo-14801450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="about-banner"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
